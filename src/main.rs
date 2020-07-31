d063a06e1c14b41f05ad3c1362ac075226d5c105
Luis Vega <vegaluisjose@gmail.com>
2020-07-31 16:41:22 -0700

    create toy program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::rc::Rc;
 use std::collections::HashMap;
 
 type Inputs = HashMap<String, i32>;
@@ -36,8 +37,55 @@ impl State {
     pub fn add_reg(&mut self, id: &str, value: i32) {
         self.regs.insert(id.to_string(), value);
     }
+
+    pub fn add_temp(&mut self, id: &str, value: i32) {
+        self.temps.insert(id.to_string(), value);
+    }
+
+    pub fn get(&self, id: &str) -> i32 {
+        if let Some(input) = self.inputs.get(id) {
+            *input
+        } else if let Some(reg) = self.regs.get(id) {
+            *reg
+        } else if let Some(temp) = self.temps.get(id) {
+            *temp
+        } else {
+            panic!("Error: {} id not found", id)
+        }
+    }
+}
+
+type Id = String;
+
+#[derive(Clone, Debug)]
+pub enum Expr {
+    Lit(i32),
+    Ref(Id),
+    Add(Rc<Expr>, Rc<Expr>),
+}
+
+#[derive(Clone, Debug)]
+pub struct Stmt {
+    pub id: Id,
+    pub expr: Expr,
+}
+
+#[derive(Clone, Debug)]
+pub struct Prog {
+    pub body: Vec<Stmt>,
+}
+
+fn add_two_inputs() -> Prog {
+    let a = Expr::Ref("a".to_string());
+    let b = Expr::Ref("b".to_string());
+    let add = Expr::Add(Rc::new(a), Rc::new(b));
+    let stmt = Stmt { id: "y".to_string(), expr: add };
+    Prog { body: vec![stmt] }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let prog = add_two_inputs();
+    for stmt in prog.body.iter() {
+        println!("{:?}", stmt);
+    }
 }
